10d82f4177ae6e80226d1d153c33691be7d0a710
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-09-10 07:04:04 +0300

    Update .gitignore

--- lostlocate/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from users.views import login_user, user_register, verify_code, verify_otp
-
-urlpatterns = [
-    path("login/", login_user, name="login"),
-    path("verify-otp/", verify_otp, name="verify_otp"),
-    path("register/", user_register, name="register"),
-    path("verify-code/", verify_code, name="verify_code"),
-]
