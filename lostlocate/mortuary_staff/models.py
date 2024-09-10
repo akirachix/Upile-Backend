56ff1bff15c9bda5654f86155106025097296183
Sharon-Njunge <your_email@example.com>
2024-09-10 14:05:18 +0300

    add mortuary_staff models

--- lostlocate/mortuary_staff/models.py
+++ lostlocate/mortuary_staff/models.py
@@ -8,10 +8,10 @@ class MortuaryStaff(models.Model):
     Model representing staff members working at a mortuary.
     """
     staff_id = models.SmallIntegerField(primary_key=True)
-    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     position = models.CharField(max_length=30)
     contact = models.CharField(max_length=15)
-     # mortuary_id = models.ForeignKey(Mortuary, on_delete=models.CASCADE)
+    mortuary_id = models.ForeignKey(Mortuary, on_delete=models.CASCADE)
     generated_code = models.CharField(max_length=30)
 
     def __str__(self):
