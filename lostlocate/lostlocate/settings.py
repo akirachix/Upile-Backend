e2bb9964aa11082e49a4dc86582eaacbbdc3bebe
Hannah-Unyolo <unyolohannah@gmail.com>
2024-09-10 08:27:09 +0300

    added missing persons model and endpoints

--- lostlocate/lostlocate/settings.py
+++ lostlocate/lostlocate/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'missing_persons',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
