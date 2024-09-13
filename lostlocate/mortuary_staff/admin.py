d87bb2d1fd43aa33a3fc71450cef6a1763f93d6b
Nataly-Wambui <wambuinatalygakenye@gmail.com>
2024-09-13 11:17:31 +0300

    adds stations/models.py

--- lostlocate/mortuary_staff/admin.py
+++ lostlocate/mortuary_staff/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
-from .models import MortuaryStaff
-
-""" Register the MortuaryStaff model with the Django admin site"""
-admin.site.register(MortuaryStaff)
-
-# Import the admin site module from Django
-from django.contrib import admin
-
 """ Import the MortuaryStaff model from the current application's models"""
-from .models import MortuaryStaff
+# from .models import MortuaryStaff
 
-""" Register the MortuaryStaff model with the Django admin site"""
-""" This allows MortuaryStaff instances to be managed through the Django admin interface"""
-admin.site.register(MortuaryStaff)
-
+# """ Register the MortuaryStaff model with the Django admin site"""
+# """ This allows MortuaryStaff instances to be managed through the Django admin interface"""
+# admin.site.register(MortuaryStaff)
