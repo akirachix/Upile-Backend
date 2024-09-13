f476d0cff416a6ec6babd25ca63b51a8dccaed47
Nataly-Wambui <wambuinatalygakenye@gmail.com>
2024-09-13 11:39:07 +0300

    fix admin

--- lostlocate/next_of_kin/admin.py
+++ lostlocate/next_of_kin/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import NextOfKin
 
-from .models import NextOfKin
-
 # Register your models here.
-admin.site.register(NextOfKin)
-admin.site.register(NextOfKin)
+# admin.site.register(NextOfKin)
+
 
--- lostlocate/unidentified_bodies/admin.py
+++ lostlocate/unidentified_bodies/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from .models import UnidentifiedBody
 
-from .models import UnidentifiedBody
+# admin.site.register(UnidentifiedBody)
 
-# Register your models here.
-admin.site.register(UnidentifiedBody)
-
-admin.site.register(UnidentifiedBody)
-
+# 
